2bb59bc8e4b4fba4abcece27797d09202cd60c69
javier salgado <javiersalgado@javiers-MacBook-Pro.local>
2018-04-25 23:41:12 -0500

    LeagueVC button created and currently working

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+	
+	var player: Player!
 
+	@IBOutlet weak var nextBtn: BorderButton!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		nextBtn.isHidden = true
+        player = Player()
     }
-
+	
+	
+	
+	
+	
 	@IBAction func onNextTapped(_ sender: Any) {
 		performSegue(withIdentifier: "skillVCSegue", sender: self)
 	}
+	
+	@IBAction func onMenTapped(_ sender: Any) {
+		selectLeague(leagueType: "men")
+	}
+	
+	@IBAction func onWomenTapped(_ sender: Any) {
+		selectLeague(leagueType: "women")
+	}
+	
+	@IBAction func onCoedTapped(_ sender: Any) {
+		selectLeague(leagueType: "coed")
+	}
 
+	
+	
+	func selectLeague(leagueType: String){
+		player.desiredLeague = leagueType
+		nextBtn.isHidden = false
+		nextBtn.isEnabled = true
+	}
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by javier salgado on 4/25/18.
+//  Copyright © 2018 javier salgado. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+	var desiredLeague: String?
+	var selectedSkillLevel: String?
+}
